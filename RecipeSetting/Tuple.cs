24daad506966430b1b81fd9675cb6e364593c95f
xiongxiao <xiong.xiao@hermes-microvision.com>
2017-04-07 18:39:44 -0700

    Tuples and User Functions

---
+++ RecipeSetting/Tuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace RecipeSetting
+{
+    class Tuple
+    {
+        private List<UserFunction> _userFunctionList;
+        private Hashtable _htTablePicks;
+        
+        public Tuple(string VEEFileName, List<string> FuncNameList)
+        {
+            _userFunctionList = new List<UserFunction>();
+            for (int i = 0; i < FuncNameList.Count; i++ )
+                _userFunctionList.Add(new UserFunction(VEEFileName, FuncNameList[i], i));
+        }
+
+        public void SetFunctionInput(int index, string InputName, string Value)
+        {
+            _userFunctionList[index].SetValue(InputName, Value);
+        }
+
+
+    }
+}
--- RecipeSetting/UserFunction.cs
+++ RecipeSetting/UserFunction.cs
@@ -6,19 +6,30 @@ using System.Collections;
 
 namespace RecipeSetting
 {
+    // User Function is the smallest element in the Project
+    // When Loading a userfunction, all input output names are automatically resolved
+    // User need to Set each input manually, then call user function
+    // All inputs and outputs [name, value] are saved in a public hashtable: Variables.
     public class UserFunction
     {
         public string VEEName { get { return _veeName; } }
         public string UserFunctionName { get { return _userFunctionName; } }
+        public Hashtable Variables { get { return _htVariables; } }
+        public int Index { get { return _index; } }
 
         private string[] _inputNames;
         private string[] _outputNames;
         private Hashtable _htVariables;
-        string _veeName;
-        string _userFunctionName;
+        private string _veeName;
+        private string _userFunctionName;
+        private int _index;
 
-        public UserFunction(string veeFileName, string userFuncName)
+        public UserFunction(string theVeeFileName, string theUserFuncName, int theIndex)
         {
+            _veeName = theVeeFileName;
+            _userFunctionName = theUserFuncName;
+            _index = theIndex;
+
             //Get All Input names
             _inputNames = null;
 
@@ -37,10 +48,14 @@ namespace RecipeSetting
             _htVariables[InputName] = Value;
         }
 
-        public string[] CallUserFunction()
+        public void CallUserFunction()
         {
-            // Calls User Function
-            return null;
+            // Calls User Function, and output Values are Saved into Hashtable
+            string[] outputs = null;
+            for (int i = 0; i < _outputNames.Length; i++)
+            {
+                _htVariables[_outputNames[i]] = outputs[i];
+            }
         }
     }
 }
